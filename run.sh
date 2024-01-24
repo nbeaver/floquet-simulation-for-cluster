79530b25343686b5a141878c3b2eee74dc3fd6ca
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2024-01-24 00:46:42 -0500

    Runscript for the long job.

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --time=01:30:00
-#SBATCH --job-name=test
+#SBATCH --time=24:00:00
+#SBATCH --job-name=odmr_floquet
 #SBATCH --partition=short
 module load anaconda3/2022.05
 LOG=time_$(date +%F_%s_%N).txt
 /usr/bin/time --output=${LOG} --verbose \
-python3 odmr_floquet_simulation.py
+python3 monte_carlo_odmr_floquet_B_x.py --verbose \
+  --out-dir='/scratch/n.beaver/2024-01-23'
