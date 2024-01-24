ebd05af71257ae74512ad7db0490d8b715ef816e
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2024-01-24 13:23:59 -0500

    Update slurm script for e.g. sending email.

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
+#! /bin/bash
 #SBATCH --nodes=1
 #SBATCH --time=24:00:00
-#SBATCH --job-name=odmr_floquet
+#SBATCH --job-name=odmr_floquet_B_z
 #SBATCH --partition=short
+#SBATCH --mail-type=ALL
 module load anaconda3/2022.05
+OUTDIR=/scratch/n.beaver/2024-01-24
 LOG=time_$(date +%F_%s_%N).txt
+mkdir -p "${OUTDIR}"
 /usr/bin/time --output=${LOG} --verbose \
-python3 monte_carlo_odmr_floquet_B_x.py --verbose \
-  --out-dir='/scratch/n.beaver/2024-01-23'
+python3 monte_carlo_odmr_floquet_B_z.py --verbose \
+  --out-dir="${OUTIDR}"
+  --tag="${SLURM_JOB_ID}"
