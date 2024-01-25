9b599bdab20033b8d2aa5db5e89832e559dd1ce8
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2024-01-24 23:00:24 -0500

    Allocate more memory for the CPU based on prior memory usage.

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@
 #SBATCH --partition=short
 #SBATCH --kill-on-invalid-dep=yes
 #SBATCH --mail-type=ALL
+#SBATCH --mem-per-cpu=4100MB
 module load anaconda3/2022.05
 OUTDIR=/scratch/n.beaver/2024-01-24
 LOG=time_$(date +%F_%s_%N).txt
