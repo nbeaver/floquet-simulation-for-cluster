7df13a6f6a0a680fe412ea2ffb124d1e9f65076f
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2024-01-25 17:21:34 -0500

    Modify for running the M_x script.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #SBATCH --nodes=1
 #SBATCH --time=24:00:00
-#SBATCH --job-name=B_z_odmr_floquet
+#SBATCH --job-name=M_x_odmr_floquet
 #SBATCH --partition=short
 #SBATCH --kill-on-invalid-dep=yes
 #SBATCH --mail-type=ALL
@@ -11,6 +11,6 @@ OUTDIR=/scratch/n.beaver/2024/${SLURM_JOB_ID}
 LOG=time_$(date +%F_%s_%N).txt
 mkdir -p "${OUTDIR}"
 /usr/bin/time --output=${LOG} --verbose \
-python3 monte_carlo_odmr_floquet_B_z.py --verbose \
+python3 monte_carlo_odmr_floquet_M_x.py --verbose \
   --out-dir="${OUTDIR}" \
   --tag="${SLURM_JOB_ID}"
