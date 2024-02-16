b2ad39b7003b88996cb9cd88b5d40b30d4c3bc20
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2024-02-16 16:55:58 -0500

    Scripts for varying Mx.

---
+++ Mx_center_0_with_Bz.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --job-name=M_x_center_0_with_Bz
+#SBATCH --partition=short
+#SBATCH --kill-on-invalid-dep=yes
+#SBATCH --mail-type=ALL
+#SBATCH --mem-per-cpu=10GB
+module load anaconda3/2022.05
+OUTDIR=$HOME/archive/2024/${SLURM_JOB_ID}
+LOG=time_out/time_$(date +%F_%s_%N).txt
+mkdir -p "${OUTDIR}"
+
+# Save environment and job information to local directory.
+local_dir=./slurm_job_info/${SLURM_JOB_ID}
+mkdir -p "${local_dir}"
+cp "$0" "${local_dir}"
+env > "${local_dir}/env.txt"
+echo "$0" > "${local_dir}/info.txt"
+echo "$*" >> "${local_dir}/info.txt"
+
+/usr/bin/time --output=${LOG} --verbose \
+python3 monte_carlo_odmr_floquet_M_x.py --verbose \
+  --out-dir="${OUTDIR}" \
+  --tag="${SLURM_JOB_ID}" \
+  --param-start=0*pi*2e6 \
+  --param-stop=1*pi*2e6 \
+  --param-steps=51 \
+  --mu-Mx=0*pi*2e6 \
+  --Bx=0.0 \
+  --By=0.0 \
+  --Bz=5.0e-4 \
+  --omega-rf-power=0.0 \
+  --omega-rf=0.0 \
+  --n-avg=300
