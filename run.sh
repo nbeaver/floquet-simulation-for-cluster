8c66fac378feb883d266b9ab1cf35f77aeb9fabe
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2024-02-22 18:17:35 -0500

    Set these as executable.

--- run.sh
+++ run.sh
@@ -2,6 +2,12 @@
 
 for i in {1..10}
 do 
-  sbatch Bz_center_0_with_Mx.sh
+  sbatch sbatch_Bz_center_0_with_Mx.sh
+  sbatch sbatch_Bz_center_5_no_Mx.sh
+  sbatch sbatch_Bz_center_5_with_Mx.sh
+  sbatch sbatch_Mx_center_0_with_Bz.sh
+  sbatch sbatch_Mx_center_2_no_Bz.sh
+  sbatch sbatch_Mx_center_2_with_Bz.sh
+  sbatch sbatch_Mx_center_5_no_Bz.sh
   sleep 1
 done
