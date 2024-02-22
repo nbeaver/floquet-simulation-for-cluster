fca7dd88cad3cb6eea000911a1c0454f0a95ccb2
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2024-02-22 13:36:21 -0500

    First script for with RF.

---
+++ with_RF_Mx_center_5_small_Bz.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --job-name=with_RF_M_x_center_5_small_Bz
+#SBATCH --partition=short
+#SBATCH --kill-on-invalid-dep=yes
+#SBATCH --mail-type=ALL
+#SBATCH --mem-per-cpu=10GB
+module load anaconda3/2022.05
+OUTDIR=$HOME/archive/2024/${SLURM_JOB_ID}
+LOG=time_out/time_$(date +%F_%s_%N).txt
+mkdir -p "${OUTDIR}"
+
+# Save environment and job information to local directory.
+local_dir=./slurm_job_info/${SLURM_JOB_ID}
+mkdir -p "${local_dir}"
+cp "$0" "${local_dir}"
+env > "${local_dir}/env.txt"
+echo "$0" > "${local_dir}/info.txt"
+echo "$*" >> "${local_dir}/info.txt"
+
+/usr/bin/time --output=${LOG} --verbose \
+python3 monte_carlo_odmr_floquet_M_x.py --verbose \
+  --out-dir="${OUTDIR}" \
+  --tag="${SLURM_JOB_ID}" \
+  --param-start=0*pi*2e6 \
+  --param-stop=1*pi*2e6 \
+  --param-steps=51 \
+  --mu-Mx=5*pi*2e6 \
+  --Bx=0.0 \
+  --By=0.0 \
+  --Bz=0.3e-4 \
+  --omega-rf-power=2*pi*2e6 \
+  --omega-rf=10*pi*2e6 \
+  --n-avg=100
