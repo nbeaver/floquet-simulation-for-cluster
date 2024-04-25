2986f7cf9aa5535caab5b4f43af72f208feeab93
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2024-04-25 10:58:08 -0400

    Go out further.

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 for i in {1..20}
 do 
-  sbatch sbatch_D_GS_small_with_Mx.sh
-  sbatch sbatch_with_RF_D_GS_small_with_Mx.sh
+    sbatch sbatch_D_GS_center2p87_with_Mx.sh
+    sbatch sbatch_with_RF_D_GS_center_2p87_with_Mx.sh
 done
--- sbatch_Bx_center_0_with_Mx.sh
+++ sbatch_Bx_center_0_with_Mx.sh
@@ -24,8 +24,8 @@ echo "$*" >> "${local_dir}/info.txt"
 python3 monte_carlo_odmr_floquet_B_x.py --verbose \
   --out-dir="${OUTDIR}" \
   --tag="${SLURM_JOB_ID}" \
-  --param-start=1.0e-4 \
-  --param-stop=0.0e-4 \
+  --param-start=3.0e-4 \
+  --param-stop=2.0e-4 \
   --param-steps=51 \
   --mu-Bx=0.0e-4 \
   --Mx='2*pi*5e6' \
